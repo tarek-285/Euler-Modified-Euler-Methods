4e12e7a532ce687ac9dd1feafc8a85484dd6a3d0
NuraNusratJannat <nuranusratjannat@gmail.com>
2026-01-14 11:06:36 +0600

    Update ODEFunction.h
    Add

--- Include/ODEFunction.h
+++ Include/ODEFunction.h
@@ -1,1 +1,13 @@
+//part 1
+
+#ifndef ODE_FUNCTIONS_H
+#define ODE_FUNCTIONS_H
+
+#include <cmath>
+#include <functional>
+
+// Definition of the ODE type: dy/dt = f(t, y)
+using Func = std::function<double(double, double)>;
+// Definition of the Exact Solution type: y = f(t)
+using ExactFunc = std::function<double(double)>;
 
